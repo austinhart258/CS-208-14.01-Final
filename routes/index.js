367d8999a105a1f841990bfcd1d79f353b51ba0c
austinhart258 <austinhart258@u.boisestate.edu>
2026-04-30 05:49:32 +0000

    Fixed image overlap plus get route twice in index.js and added index to .hero class + updated donut image

--- routes/index.js
+++ routes/index.js
@@ -15,58 +15,17 @@ router.get('/about', function(req, res) {
   res.render('index', { title: 'About', page: 'about' });
 });
 
-/* GET home page. */
-router.get('/', function(req, res, next){
-  const page = req.query.page || 'home';
+/* COMMENTS */
+router.get('/comments', function(req, res) {
+  req.db.query('SELECT * FROM todos ORDER BY id DESC', (err, results) => {
+    if (err) return res.status(500).send("DB error");
 
-  try {
-    req.db.query('SELECT * FROM todos;', (err, results) => {
-      if (err) {
-        console.error('Error fetching todos:', err);
-        return res.status(500).send('Error fetching todos');
-      }
-      res.render('index', { title: 'Downtown Donuts', todos: results, page: page });
+    res.render('index', {
+      title: 'Comments',
+      page: 'comments',
+      todos: results
     });
-  } catch (error) {
-    console.error('Error fetching items:', error);
-    res.status(500).send('Error fetching items');
-  }
-});
-
-router.post('/create', function (req, res, next) {
-    const { task } = req.body;
-    try {
-      req.db.query('INSERT INTO todos (task) VALUES (?);', [task], (err, results) => {
-        if (err) {
-          console.error('Error adding todo:', err);
-          return res.status(500).send('Error adding todo');
-        }
-        console.log('Todo added successfully:', results);
-        // Redirect to the home page after adding
-        res.redirect('/');
-      });
-    } catch (error) {
-      console.error('Error adding todo:', error);
-      res.status(500).send('Error adding todo');
-    }
-});
-
-router.post('/delete', function (req, res, next) {
-    const { id } = req.body;
-    try {
-      req.db.query('DELETE FROM todos WHERE id = ?;', [id], (err, results) => {
-        if (err) {
-          console.error('Error deleting todo:', err);
-          return res.status(500).send('Error deleting todo');
-        }
-        console.log('Todo deleted successfully:', results);
-        // Redirect to the home page after deletion
-        res.redirect('/');
-    });
-    }catch (error) {
-        console.error('Error deleting todo:', error);
-        res.status(500).send('Error deleting todo:');
-    }
+  });
 });
 
 module.exports = router;
