09b8b4cccd3dd82c11a8a6f9d39d90bfa37a1a39
austinhart258 <austinhart258@u.boisestate.edu>
2026-04-30 06:56:26 +0000

    Added pagination + backend/frontend handling as well as some edge case handling though I still need to add more to include timestamps

--- routes/index.js
+++ routes/index.js
@@ -17,15 +17,64 @@ router.get('/about', function(req, res) {
 
 /* COMMENTS */
 router.get('/comments', function(req, res) {
-  req.db.query('SELECT * FROM todos ORDER BY id DESC', (err, results) => {
-    if (err) return res.status(500).send("DB error");
+  const pageNum = parseInt(req.query.p) || 1;
+  const limit = 5;
+  const offset = (pageNum - 1) * limit;
 
-    res.render('index', {
-      title: 'Comments',
-      page: 'comments',
-      todos: results
+  req.db.query(
+    'SELECT * FROM todos ORDER BY id DESC LIMIT ? OFFSET ?',
+    [limit, offset],
+    (err, results) => {
+      if (err) return res.render('index', { page: 'comments', todos: [], error: "Server is currently unavailable. Please try again later."});
+
+      res.render('index', {
+        title: 'Comments',
+        page: 'comments',
+        todos: results,
+        currentPage: pageNum
+      });
+    }
+  );
+});
+
+router.post('/add-comment', function (req, res) {
+  let { task } = req.body;
+
+  /* MAKE SURE TODO EXISTS FOR ERROR HANDLING */
+  const reloadComments = () => {
+    req.db.query('SELECT * FROM todos ORDER BY id DESC', (err, results) => {
+      res.render('index', {
+        title: 'Comments',
+        page: 'comments',
+        todos: results || [],
+        error: "Something went wrong"
+      });
     });
-  });
+  };
+
+  /* VALIDATION */
+  if (!task || task.trim() === '') {
+    return reloadComments();
+  }
+
+  if (task.length > 300) {
+    return reloadComments();
+  }
+
+  const clean = task.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+
+  /* INSERT TASK */
+  req.db.query(
+  'INSERT INTO todos (task) VALUES (?)',
+  [clean],
+  (err) => {
+    if (err) {
+      console.error(err);
+      return reloadComments();
+    }
+    res.redirect('/comments');
+  }
+);
 });
 
 module.exports = router;
